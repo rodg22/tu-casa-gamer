d1d1470f752b69fd2ada5b14c2dee82ec38172f8
Rodrigo Giudice <giudicerodrigo@hotmail.com>
2021-11-21 01:05:45 -0300

    Agregando cards con MUI

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,10 +1,35 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
 export default function Item({title, description, price, pictureUrl}) {
-    return(
-        <div class="item">
-            <img src={pictureUrl} alt="imagendeproducto" />
-            <h2>{title}</h2>
-            <h3>{description}</h3>
-            <p>US$ {price}</p>
-        </div>
-    )
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        height="140"
+        image={pictureUrl}
+        alt={title}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+        {title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+        {description}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+        US$ {price}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Agregar al carrito</Button>
+        <Button size="small">Ver detalle</Button>
+      </CardActions>
+    </Card>
+  );
 }
--- src/services/ItemPromise.jsx
+++ src/services/ItemPromise.jsx
@@ -1,7 +1,7 @@
 const itemsData = [
     {
         "id":1,
-        "pictureUrl":"./assets",
+        "pictureUrl":"https://assets.nintendo.com/image/upload/b_white,c_pad,f_auto,h_382,q_auto,w_573/ncom/es_LA/hardware/switch/nintendo-switch-neon-new-package/gallery/image01?v=2021111513",
         "title":"Nintendo Switch",
         "description": "switch",
         "category":"Consolas",
@@ -10,7 +10,7 @@ const itemsData = [
     },
     {
         "id":2,
-        "pictureUrl":"",
+        "pictureUrl":"https://images.fravega.com/f500/e24171063a6f9d21eb89c7d9eae6bc5a.jpg",
         "title":"Playstation 5",
         "description": "ps5",
         "category":"Consolas",
@@ -19,8 +19,8 @@ const itemsData = [
     },
     {
         "id":3,
-        "pictureUrl":"",
-        "title":"X-box",
+        "pictureUrl":"https://cdn.alfabetajuega.com/wp-content/uploads/2020/11/1604491465_802334_1604582279_noticia_normal-780x405.jpg",
+        "title":"X-box S series",
         "description": "x-box",
         "category":"Consolas",
         "price": "400",
@@ -28,7 +28,7 @@ const itemsData = [
     },
     {
         "id":4,
-        "pictureUrl":"",
+        "pictureUrl":"https://img.redbull.com/images/c_limit,w_1500,h_1000,f_auto,q_auto/redbullcom/2021/7/28/pmjkmhqo940brpe98wz6/fifa-22-logo-mbappe",
         "title":"FIFA 22",
         "description": "Football game",
         "category":"Juegos de Playstation 5",
@@ -37,7 +37,7 @@ const itemsData = [
     },
     {
         "id":5,
-        "pictureUrl":"",
+        "pictureUrl":"https://www.masgamers.com/wp-content/uploads/2017/01/NintendoSwitch_TLOZBreathoftheWild_boxart_SmallBundle-e1484295047121.jpg",
         "title":"Zelda: Breath of the wild",
         "description": "juego de switch",
         "category":"Juegos de Nintendo Switch",
